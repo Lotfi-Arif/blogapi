b7bc27a8639949400ef30b831713d1678b1791e9
Lotfi Arif <Luffyarif58@gmail.com>
2022-01-23 19:55:14 +0800

    Add categories resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: []
+  imports: [PrismaModule, UsersModule, CategoriesModule]
 })
 export class AppModule {}
