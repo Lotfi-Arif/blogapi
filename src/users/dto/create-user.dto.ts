a9aa545770c9e1fd2d625d3604925bcdb665632c
Lotfi Arif <Luffyarif58@gmail.com>
2022-01-24 00:49:25 +0800

    Add posts module and validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,18 @@
-export class CreateUserDto {
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator"
 
+export class CreateUserDto {
+    @MinLength(3)
+    @IsNotEmpty()
+    @IsString()
     name: string
+    @MinLength(3)
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
     email: string
+    @MinLength(3)
+    @IsNotEmpty()
+    @IsString()
     password: string
 
 }
